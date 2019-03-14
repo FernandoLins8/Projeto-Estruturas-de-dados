631f00bd2190fbf110b95f30be784aee007e0e25
Fernando Lins <FernandoLins088@gmail.com>
2019-03-13 23:16:59 -0300

    creates priority queue, huff tree

--- Huffman/huffman.c
+++ Huffman/huffman.c
@@ -32,6 +32,15 @@ typedef struct hash_table
 } Hash_table;
 
 
+Node* create_node(unsigned char value, int priority, Node *next, Node *left, Node *right);
+Queue* create_priority_queue();
+void enqueue(Queue *pq, Node *node);
+Node* dequeue(Queue *pq);
+void print_queue(Queue *pq);
+Node* create_tree(Node *left, Node *right);
+void print_tree(Node *tree);
+
+
 int main(int argc, char const *argv[])
 {
 	if(argc != 2)
@@ -53,5 +62,127 @@ int main(int argc, char const *argv[])
 		freq[ch]++;
 		fread(&ch, sizeof(ch), 1, input_file);
 	}
+
+	Queue *pq = create_priority_queue();
+
+	for(i=0; i<MAX; i++)
+		if(freq[i])
+		{
+			enqueue(pq, create_node(i, freq[i], NULL, NULL, NULL));
+		}
+	//print_queue(pq);
+
+	for(;;)
+	{
+		Node *n1 = dequeue(pq),
+			 *n2 = dequeue(pq);
+		Node *new = create_tree(n1, n2);
+
+		if(!pq->head)
+		{
+			enqueue(pq, new);
+			break;
+		}
+		enqueue(pq, new);
+	}
+
+	//print_tree(pq->head);
 	return 0;
+}
+
+
+Node* create_node(unsigned char value, int priority, Node *next, Node *left, Node *right)
+{
+	Node *new = malloc(sizeof(Node));
+	new->value = value;
+	new->priority = priority;
+
+	if(new->value == 42 || new->value == 92)
+		new->control = 1;
+	else
+		new->control = 0;
+
+	new->next = next;
+	new->left = left;
+	new->right = right;
+
+	return new;
+}
+
+Queue* create_priority_queue()
+{
+	Queue *new = malloc(sizeof(Queue));
+	new->head = NULL;
+
+	return new;
+}
+
+void enqueue(Queue *pq, Node *node)
+{
+	if(!pq->head || pq->head->priority >= node->priority)
+	{
+		node->next = pq->head;
+		pq->head = node;
+	}
+	else
+	{
+		Node *n = pq->head;
+		while(n->next && n->next->priority < node->priority)
+			n = n->next;
+
+		node->next = n->next;
+		n->next = node;
+	}
+}
+
+Node* dequeue(Queue *pq)
+{
+	if(!pq->head)
+		return NULL;
+	else
+	{
+		Node *removed = pq->head;
+		pq->head = pq->head->next;
+
+		return removed;
+	}
+}
+
+void print_queue(Queue *pq)
+{
+	if(!pq->head)
+	{
+		printf("Empty queue\n");
+		return;
+	}
+
+	Node *n = pq->head;
+	while(n)
+	{
+		printf("%c,%d ", n->value, n->priority);
+		n = n->next;
+	}
+	printf("\n");
+}
+
+Node* create_tree(Node *left, Node *right)
+{
+	Node *new = malloc(sizeof(Node));
+	new->value = '*';
+	new->priority = left->priority + right->priority;
+
+	new->left = left;
+	new->right = right;
+
+	return new;
+}
+
+void print_tree(Node *tree)
+{
+	if(tree)
+	{
+		printf("%c ", tree->value);
+		print_tree(tree->left);
+		print_tree(tree->right);
+	}
 }
