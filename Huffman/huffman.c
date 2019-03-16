5708f6d3738ffcc3616b9a14c07fbc6be9e5510e
Fernando Lins <35555623+FernandoLins8@users.noreply.github.com>
2019-03-16 16:05:31 -0300

    Now writing two first bytes and tree

--- Huffman/huffman.c
+++ Huffman/huffman.c
@@ -23,6 +23,7 @@ typedef struct element
 {
 	int freq;
 	unsigned char *byte;
+	unsigned char new_enc;
 	int size;
 } Element;
 
@@ -38,14 +39,18 @@ void enqueue(Queue *pq, Node *node);
 Node* dequeue(Queue *pq);
 void print_queue(Queue *pq);
 Node* create_tree(Node *left, Node *right);
-void print_tree(Node *tree);
+void write_tree(Node *node, FILE *output_file);
 
 Hash_table* create_hash_table();
+void create_new_enc(Element *element);
 int height(Node *node);
 void search(Hash_table *ht, Node* node, unsigned char *byte, int size);
 int get_tree_size(Node *node);
 int get_data_size(Hash_table *ht);
 
+unsigned char set_bit(unsigned char c, int i);
+
+void compress(FILE *input_file, Hash_table *ht, Node* tree, int tree_size, int data_size);
 
 int main(int argc, char const *argv[])
 {
@@ -65,6 +70,7 @@ int main(int argc, char const *argv[])
 	fread(&ch, sizeof(ch), 1, input_file);
 	while(!feof(input_file))
 	{
+		total_char++;
 		if(ch != 10)
 			freq[ch]++;
 		fread(&ch, sizeof(ch), 1, input_file);
@@ -98,11 +104,11 @@ int main(int argc, char const *argv[])
 		enqueue(pq, new);
 	}
 
-	//print_tree(pq->head);
+	//write_tree(pq->head, NULL);
 
 	Hash_table *ht = create_hash_table();
 
-	unsigned char *byte = malloc(sizeof(unsigned char)*height(pq->head)+1);
+	unsigned char *byte = malloc(sizeof(unsigned char)*height(pq->head));
 	search(ht, pq->head, byte, 0);
 
 	/*Print Hash
@@ -115,10 +121,14 @@ int main(int argc, char const *argv[])
 	}*/
 
 	int tree_size = get_tree_size(pq->head),
-	data_size = get_data_size(ht),
-	trash_size = (tree_size + data_size) % 8;
+	data_size = get_data_size(ht);
 
-	printf("tree_size = %d\ndata_size = %d\ntrash_size = %d\n", get_tree_size(pq->head), get_data_size(ht), trash_size);
+	//printf("%u\n", trash_size);
+
+	//printf("tree_size = %d\ndata_size = %d\ntrash_size = %d\n", get_tree_size(pq->head), get_data_size(ht), trash_size);
+
+	compress(input_file, ht, pq->head, tree_size, data_size);
+
 
 	return 0;
 }
@@ -210,13 +220,14 @@ Node* create_tree(Node *left, Node *right)
 	return new;
 }
 
-void print_tree(Node *tree)
+void write_tree(Node *node, FILE *output_file)
 {
-	if(tree)
+	if(node)
 	{
-		printf("%c ", tree->value);
-		print_tree(tree->left);
-		print_tree(tree->right);
+		fwrite(&node->value, 1, 1, output_file);
+		//printf("%c ", node->value);
+		write_tree(node->left, output_file);
+		write_tree(node->right, output_file);
 	}
 }
 
@@ -237,14 +248,26 @@ int height(Node *node)
 		return 0;
 	else
 	{
-		int left_height = height(node->left),
-		right_height = height(node->right);
+		int left_height = height(node->left), right_height = height(node->right);
 
 		if(left_height > right_height)
 			return left_height+1;
 		else
 			return right_height+1;
 	}
+}
+
+void create_new_enc(Element *element)
+{
+	int i = element->size;
+	unsigned char new = 0;
+	while(i)
+	{
+		if(element->byte[i-1] == '1')
+			new = set_bit(new, element->size - i);
+		i--;
+	}
+	element->new_enc = new;
 }
 
 void search(Hash_table *ht, Node* node, unsigned char *byte, int size)
@@ -262,7 +285,8 @@ void search(Hash_table *ht, Node* node, unsigned char *byte, int size)
 			ht->table[node->value]->byte = new_byte;
 			ht->table[node->value]->size = size;
 
-			//printf("%c %s\n", node->value, ht->table[node->value]->byte);
+			create_new_enc(ht->table[node->value]);
+			//printf("%c %s %hu\n", node->value, ht->table[node->value]->byte, ht->table[node->value]->new_enc);
 		}
 		else
 		{
@@ -303,4 +327,27 @@ unsigned char set_bit(unsigned char c, int i)
 {
 	unsigned char mask = 1 << i;
 	return mask | c;
+}
+
+void compress(FILE *input_file, Hash_table *ht, Node* tree, int tree_size, int data_size)
+{
+	FILE *output_file;
+	output_file = fopen("output_file", "wb");
+
+	unsigned char trash_size, first_byte, second_byte, complete_first;
+
+	trash_size = 8 - ((tree_size) % 8);
+	trash_size = trash_size << 5;
+
+	complete_first = tree_size >> 8;
+
+	first_byte = trash_size | complete_first;
+	second_byte = tree_size;
+
+	fwrite(&first_byte, 1, 1, output_file);
+	fwrite(&second_byte, 1, 1, output_file);
+
+	write_tree(tree, output_file);
+
+	rewind(input_file);
 }
